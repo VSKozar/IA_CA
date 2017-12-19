b6941d8535f4b8f0f122700256e2f082c3b1a597
Vasyl_Kozar <Vasyl_Kozar@EPUALVIW0293>
2017-12-19 19:17:09 +0200

    Steps 9-13 were Implemented.

--- IA_CA/TestScenarios/IaTests.cs
+++ IA_CA/TestScenarios/IaTests.cs
@@ -2,6 +2,9 @@
 using OpenQA.Selenium;
 using IA_CA.Core;
 using IA_CA.PageObjects;
+using static IA_CA.CustomTypes.CalculatorTypes;
+using System;
+using System.Threading;
 
 namespace IA_CA
 {
@@ -40,18 +43,30 @@ namespace IA_CA
             mortgagePaymentCalculatorPage.CleanPurchasePriceField();
             mortgagePaymentCalculatorPage.ClickPurchasePricePlusBtn(2);
 
-            // 8. Change the Down Payment to 50 000 using the + button of the slider
-            mortgagePaymentCalculatorPage.ClickDownPaymentPlusBtn(5);
+            // 8. Change the Down Payment to 40 000 using the + button of the slider
+            mortgagePaymentCalculatorPage.ClickDownPaymentPlusBtn(4);
 
             // 9. Select 15 years for Amortization
-            // TO DO ...
+            mortgagePaymentCalculatorPage.SelectAmortization(Amortization.YEARS15);
 
+            // 10. Select Weekly for Payment Frequency
+            mortgagePaymentCalculatorPage.SelectPaymentFrequency(PaymentFrequency.WEEKLY);
+
+            // 11. Change the Interest Rate to 5%
+            mortgagePaymentCalculatorPage.SpecifyInterestRate(5.0);
+
+            // 12. Click the Calculate button
+            mortgagePaymentCalculatorPage.clickCalculateBtn();
+
+            // Verify that the weekly payments value is 181.59
+            Thread.Sleep(3000);
+            StringAssert.AreEqualIgnoringCase("181.59", mortgagePaymentCalculatorPage.getPaiementResult());
         }
 
         [TearDown]
         public void AfterTest()
         {
-            //driver.Close();
+            driver.Close();
         }
     }
 }
